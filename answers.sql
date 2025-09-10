6a83ac39d36984de78ee43594670af1de030aa97
Samuel Armah <samuelarmah817@gmail.com>
2025-09-10 08:14:19 +0000

    my wk-6 assignment

---
+++ answers.sql
@@ -0,0 +1,35 @@
+-- fetching the details of all employees --
+
+SELECT 
+    e.firstName,
+    e.lastName,
+    e.email,
+    e.officeCode
+FROM employees e
+INNER JOIN offices o
+ON e.officeCode = o.officeCode;
+
+
+-- retrieving data about products --
+
+SELECT 
+    p.productName,
+    p.productVendor,
+    p.productLine
+FROM products p
+LEFT JOIN productlines pl
+ON p.productLine = pl.productLine;
+
+
+-- retrieving details of the first ten orders --
+
+SELECT 
+    o.orderDate,
+    o.shippedDate,
+    o.status,
+    o.customerNumber
+FROM customers c
+RIGHT JOIN orders o
+ON c.customerNumber = o.customerNumber
+LIMIT 10;
+
